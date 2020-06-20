d3cbd6e4d91901f11d6a9ee786df6675b662469b
KHO11 <54800683+KHO11@users.noreply.github.com>
2020-06-20 19:08:48 +0100

    Add files via upload

---
+++ QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        id :0,
+        question : "What is the capital of Nigeria?",
+        options : ["New Delhi", "Abuja", "Aba", "Onisha"],
+        answer : "Abuja"
+    },
+    {
+        id : 1,
+        question : "What is the capital city of India?",
+        options : ["Punjab", "Awka", "Owerri", "New Delhi"],
+        answer : "New Delhi"
+    },
+    {
+        id : 2,
+        question : "What is the capital city of Australia?",
+        options : ["Mumbai", "AAbuja", "Warri", "Sydney"],
+        answer : "Sydney"
+    },
+    {
+        id : 3,
+        question : "What is the captial city of Turkey",
+        options : ["Bangalore", "Ankara", "Istanbul", "Enugu"],
+        answer : "Ankara"
+    }
+]
+
